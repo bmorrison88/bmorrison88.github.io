572bb2f45ee7ad658e9d71005206f8ee462f65c8
Brandon Morrison <b.mo94@hotmail.com>
2018-04-30 22:26:49 -0500

    Update of porfolio

---
+++ portfolio.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html>
+
+<head>
+	<title>Portfolio</title>
+	<link rel = "stylesheet" href = "../css/portfolio.css">
+</head>
+<body>
+	<div id = "nav">
+		<h1 id = "title">Brandon Morrison's Portfolio</h1>
+		<?php include 'header.php';?>
+	</div>
+	
+	<hr>
+	
+	<div id = "hire-me">
+		<h2>Welcome to my portfolio!</h2>
+		<p>
+			Hello, my name is Brandon Morrison. I recently graduated from SIUE with my Bachelor's 
+			degree in Computer Science. Below you will find some of my side projects and a little bit more about me!
+		</p>
+		<a href = "../resume.pdf"><button>Hire Me!</button></a>
+	</div>
+	
+	<hr>
+	
+	<div>
+		<h2 id = "h2">Latest Projects</h2>
+		<div id = "project-container">
+			<span class = "projects"><a href = "https://bmorrison88.github.io/bmorrison88.github.io-shoppingList/">Angular Shopping List</a></span>
+			<!--  <span class = "boxes">Box 2</span>
+			<span class = "boxes">Box 3</span> -->
+		</div>
+	</div>
+	
+	<hr>
+	
+	<div id = "jumbo-container">
+		<div id= "about-me">
+			<h3>Passion for Programming</h3>
+			<p>I attended Southern Illinois University in Carbondale my first year and transferred over to 
+			their sister school - SIUE. I found my love for programming during my junior year of high school.
+			 It was a class that was quite different from any other offered, even though we did have Mandarin 
+			 as an option as well (I took Spanish instead). I was always had a love for solving problems but when 
+			 I learned that I could solve problems using a simple text editor on a computer, that love expanded beyond 
+			 its original capacity. From the day I completed my first program up until now, my passion for solving 
+			 problems continue to grow. There is no better satisfaction or adrenaline rush than completing a program, 
+			 knowing that you put a lot of effort and time into it.</p>
+		</div>
+		
+		<div id= "about-me">
+			<h3>Hobbies and Interests</h3>
+			<p></p>
+		</div>
+		
+		<div id = "whats-new">
+			<h3>What's New?</h3>
+			<div class = "little-box">
+				<span>
+					<h5>Restaurant</h5>
+				</span>
+				<p>Designing and developing a website for local restaurant.</p>
+			</div>
+			<hr>
+			<div class = "little-box">
+				<span>
+					<h5>Mobile Application Development</h5>
+				</span>
+				<p>Currently learning the fundamentals of creating an app for Android</p>
+			</div>
+		</div>
+	</div>
+	<hr>
+	<footer>
+		<h5>Copyright&#169 2018 bmorrison</h5>
+	</footer>
+</body>
+
+</html>
---
+++ portfolio/php/header.php
@@ -0,0 +1,13 @@
+<DOCTYPE HTML>
+
+<html>
+
+<div id = "contact-me">
+	<ul>
+		<li>Software Developer</li>
+		<li>C: (708)-6946-6501</li>
+		<li>brandon.morrison147@gmail.com</li>
+	</ul>
+</div>
+
+</html>
